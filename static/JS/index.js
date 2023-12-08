46eb0df087f0769af10bd6743c45ee8e01f4d73d
Nilswik <nilswik1423@gmail.com>
2023-12-08 18:11:44 +0100

    TTS och STT borde fungera kanske lol

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 var chatbox = document.getElementById("chatbox");
 // Function to handle the scroll event
 function handleScroll() {
@@ -15,6 +19,64 @@ function sendMessage() {
 
 
 //text to speach 
+
+
+
+//speech to text
+let recognition;
+let Transcription = "";
+let isreading = false;
+
+
+if ('webkitSpeechRecognition' in window) {
+    // For Chrome
+    recognition = new webkitSpeechRecognition();
+} else if ('SpeechRecognition' in window) {
+    // For Firefox
+    recognition = new SpeechRecognition();
+} else {
+    console.error('Speech recognition not supported in this browser.');
+}
+
+if (recognition) {
+    let transcription = "";
+
+    // properties för speech rec
+    recognition.lang = 'en-US';
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    // Event listener för när speec recon är klart 
+    recognition.onresult = function(event) {
+        transcription = event.results[0][0].transcript;
+        document.getElementById("UserInput").innerText = "Transcription: " + transcription;
+    };
+
+    // Error speech
+    recognition.onerror = function(event) {
+        console.error('Speech recognition error:', event.error);
+    };
+
+    // Button click event listener to start speech recognition
+    document.getElementById("startSpeechRecognition").addEventListener("click", function() {
+        recognition.start();
+    });
+
+    // Knapp för speec recon
+    document.getElementById("readTranscription").addEventListener("click", function() {
+        Text_To_Speech(transcription);
+    });
+}
+
+document.getElementById("readTranscription").addEventListener("click", function() {
+    if (!isReading) {
+        Text_To_Speech(transcription);
+    } else {
+        window.speechSynthesis.cancel(); // sluta läsa
+    }
+    
+    isreading = !isreading
+});
 
 function Text_To_Speech(text) {
     let utterance = new SpeechSynthesisUtterance();
@@ -63,6 +125,9 @@ function sendMessageHelper(userInput) {
     }
 
     Text_To_Speach(botText.innerText)
+    if (isreading){
+        sendMessageHelper(e.target.value)
+    }
 }
 
 // Function to handle key press events
@@ -71,6 +136,13 @@ document.getElementById("UserInput").addEventListener("keypress", function (e) {
         sendMessageHelper(e.target.value);
     }
 });
+
+
+document.addEventListener("keydown",function (c){
+    if (c.key == "Ö" || c.key == "ö"){
+        recognition.start();
+    }
+})
 document.querySelector("#asker button").addEventListener("click", sendMessage);
 
 
