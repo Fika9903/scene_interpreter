fe3741d05d73ffd0578f9237af689e4145e152ef
fika9903 <fkdahlgren@hotmail.com>
2023-12-21 12:49:11 +0100

    Ändrat i functioncalling så den tar fler objeckt istället för att bara söka historiken för ett objekt i taget.

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ def read_and_update_json(file_path):
                             object_history[items['Name']] = {current_time: rounded_location}
                     #print(object_history)
 
-                else:
-                    print("JSON is up to date.")
+                # else:
+                #     print("JSON is up to date.")
 
         except Exception as e:
             print(f"Error reading JSON file: {e}")
@@ -90,15 +90,78 @@ def read_and_update_json(file_path):
         time.sleep(1)  # Update interval, change as needed
 
 
-def print_object_history(arguments_json):
-        global object_history
-        arguments = json.loads(arguments_json)
-        object_name = arguments["object_name"]
+# def print_object_history(arguments_json):
+#         global object_history
+#         arguments = json.loads(arguments_json)
+#         object_name = arguments["object_name"]
 
-        if object_name in object_history:
-            return json.dumps({"history": object_history[object_name]})
+#         if object_name in object_history:
+#             return json.dumps({"history": object_history[object_name]})
+#         else:
+#             return json.dumps({"error": "Object not found"})
+def print_object_history(arguments_json):
+    global object_history
+    arguments = json.loads(arguments_json)
+    object_names = arguments["object_name"]
+    history_data = {}
+
+    for name in object_names:
+        if name in object_history:
+            history_data[name] = object_history[name]
         else:
-            return json.dumps({"error": "Object not found"})
+            history_data[name] = "Object not found"
+
+    return json.dumps({"history": history_data})
+
+
+# def format_history(data_json):
+#     # Parse the JSON string back into a dictionary
+#     data = json.loads(data_json)
+
+#     formatted_output = []
+#     header = "Object Name\tTime\t\tX Coordinate\tY Coordinate\tZ Coordinate\n" + "-"*80
+#     formatted_output.append(header)
+
+#     for object_name, timestamps in data['history'].items():
+#         # Check if timestamps is a dictionary (valid data) or a string (error message)
+#         if isinstance(timestamps, dict):
+#             for time, coordinates in timestamps.items():
+#                 row = f"{object_name}\t{time}\t{coordinates['X']}\t{coordinates['Y']}\t{coordinates['Z']}"
+#                 formatted_output.append(row)
+#         else:
+#             # Handling the case where the object history is not found
+#             formatted_output.append(f"{object_name}: {timestamps}")
+
+#     return "\n".join(formatted_output)
+
+def format_history(data_json):
+    # Parse the JSON string back into a dictionary
+    data = json.loads(data_json)
+
+    formatted_output = []
+    header = "Object Name       Time        X Coordinate    Y Coordinate    Z Coordinate"
+    separator = "-" * len(header)
+    formatted_output.append(header)
+    formatted_output.append(separator)
+
+    for object_name, timestamps in data['history'].items():
+        if isinstance(timestamps, dict):
+            for time, coordinates in timestamps.items():
+                formatted_output.append(
+                    f"{object_name:<15} {time:<10} "
+                    f"{coordinates['X']:>14,.1f} "
+                    f"{coordinates['Y']:>14,.1f} "
+                    f"{coordinates['Z']:>14,.1f}"
+                )
+        else:
+            formatted_output.append(f"{object_name:<15} {timestamps}")
+
+        # Add a separator between different objects for clarity
+        formatted_output.append(separator)
+
+    return "\n".join(formatted_output)
+
+
 
 
 def answer_question(question: str, scene, object_history) -> str:
@@ -121,9 +184,31 @@ def answer_question(question: str, scene, object_history) -> str:
         }
     ]
 
+    
+    function_description = [
+        {
+            "name": "print_object_history",
+            "description": "Retrieve and return the historical data for a list of specified objects.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "object_name": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        },
+                        "description": "A list of names of the objects for which to retrieve historical data."
+                    }
+                },
+                "required": ["object_name"]
+            }
+        }
+    ]
+
+
     if session['conversation_history'] == []:
         print('empty list, appending system message and scene context.')
-        session['conversation_history'].append({"role": "system", "content": "Your job is to examine a Scene description given in JSON format and answer a question given regarding the scene."})
+        session['conversation_history'].append({"role": "system", "content": "Your job is to examine a Scene description given in JSON format and answer a question given regarding the scene. If you use the history tool, answer the question by interpreting the historical coordinates and try to understand how the objects have moved in reality rather than explaining in terms of coordinates."})
         session['conversation_history'].append({"role": "user", "content": scene_context})
 
     session['conversation_history'].append({"role": "user", "content": question})
@@ -146,15 +231,19 @@ def answer_question(question: str, scene, object_history) -> str:
         function_args = {"object_history": object_history, "object_name": object_name}
         function_args_json = json.dumps(function_args)
         function_response = print_object_history(arguments_json=function_args_json)
-
+        reformated = format_history(function_response)
         # Append the function response to the conversation history
-        session['conversation_history'].append({"role": "function", "name": "print_object_history", "content": function_response})
+        session['conversation_history'].append({"role": "function", "name": "print_object_history", "content": reformated})
+        session['conversation_history'].append({"role": "user", "content": })
         
         # Make the second API call if needed
         second_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-1106",
             messages=session['conversation_history']
         )
+        
+        second_response_message = second_response.choices[0].message
+        print(second_response_message)
         second_response_message = second_response.choices[0].message.content.strip()
         session['conversation_history'].append({"role": "assistant", "content": second_response_message})
         session.modified = True  # Mark session as modified
